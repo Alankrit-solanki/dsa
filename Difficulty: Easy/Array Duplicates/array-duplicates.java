60fbadd35fcaaacb92b5d60aff77316f24274361
Alankrit-solanki <alankritsolanki82@gmail.com>
2026-01-31 13:21:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] arr) {
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int index = Math.abs(arr[i]) - 1;
+
+            if (arr[index] < 0) {
+                res.add(index + 1);
+            } else {
+                arr[index] = -arr[index];
+            }
+        }
+
+        return res;
+    }
+}
