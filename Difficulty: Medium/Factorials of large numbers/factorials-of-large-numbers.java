04ecc0a1bd7e25666edc0e1e253619ba16b180de
Alankrit-solanki <alankritsolanki82@gmail.com>
2026-02-05 17:47:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Factorials of large numbers/factorials-of-large-numbers.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public static ArrayList<Integer> factorial(int n) {
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(1);
+        
+        for (int i = 2; i <= n; i++) {
+            multiply(result, i);
+        }
+        
+        Collections.reverse(result);
+        return result;
+    }
+    
+    static void multiply(ArrayList<Integer> result, int x) {
+        int carry = 0;
+        
+        for (int i = 0; i < result.size(); i++) {
+            int product = result.get(i) * x + carry;
+            
+            result.set(i, product % 10);
+            carry = product / 10;       
+        }
+        
+        while (carry > 0) {
+            result.add(carry % 10);
+            carry /= 10;
+        }
+    }
+}
