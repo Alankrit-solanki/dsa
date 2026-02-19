733c7771818252a7b430717339c836fd5867de81
Alankrit-solanki <alankritsolanki82@gmail.com>
2026-02-19 23:59:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Three way partitioning/three-way-partitioning.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void threeWayPartition(int arr[], int a, int b) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        
+        while (mid <= high) {
+            if (arr[mid] < a) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] > b) {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            } else {
+                mid++;
+            }
+        }
+    }
+}
